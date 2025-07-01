7a941442e956461e63f7170d9c479690d9e1bc60
Igor <ra200246@ucdb.br>
2025-07-01 12:35:28 -0400

    Initial commit

---
+++ BeerSender.Domain/Boxes/Box.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeerSender.Domain.Boxes
+{
+    public class Box : AggregateRoot
+    {
+        public void Apply(BoxCreated @event)
+        {
+            Capacity = @event.Capacity;
+        }
+        public void Apply(ShippingLabelAdded @event)
+        {
+            ShippingLabel = @event.Label;
+        }
+        public BoxCapacity? Capacity{ get; private set; }
+        public ShippingLabel? ShippingLabel { get; private set; }
+    }
+
+    public record CreateBox(
+    Guid BoxId,
+    int DesiredNumberOfSpots
+    );
+
+    public record AddShippingLabel(
+        Guid BoxId,
+        string TrackingCode,
+        Carrier Carrier);
+
+    public record BoxCreated(
+    BoxCapacity Capacity
+    );
+
+    public record ShippingLabelAdded(
+        ShippingLabel Label);
+
+    public record ShippingLabelFailedToAdd(ShippingLabelFailedToAdd.FailReason Reason)
+    {
+        public enum FailReason
+        {
+            TrackingCodeInvalid,
+        }
+    };
+
+    public enum Carrier
+    {
+        UPS,
+        FedEx,
+        BPost
+    }
+
+    public record ShippingLabel(Carrier Carrier, string TrackingCode)
+    {
+        private bool IsValid()
+        {
+            return Carrier switch
+            { 
+                Carrier.UPS => TrackingCode.StartsWith("ABC"),
+                Carrier.FedEx => TrackingCode.StartsWith("DEF"),
+                Carrier.BPost => TrackingCode.StartsWith("GHI"),
+                _ => throw new ArgumentOutOfRangeException(nameof(Carrier), Carrier, null),
+            };
+        }
+    }
+
+    public record BoxCapacity(int NumberOfSpots)
+    {
+        public static BoxCapacity Create(int desiredNumberOfSpots)
+        {
+            return desiredNumberOfSpots switch
+            {
+                <= 6 => new BoxCapacity(6),
+                <= 12 => new BoxCapacity(12),
+                _ => new BoxCapacity(24),
+            };
+        }
+    }
+}
