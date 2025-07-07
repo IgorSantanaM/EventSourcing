2fa270d8d63c8c4d4eb83c499c75f270d5089ee2
IgorSantanaM <igorsantanamedeiros17@gmail.com>
2025-07-06 21:11:34 -0400

    (feat): Added class fixture for marten testing

---
+++ BeerSender.Domain.Tests/MartenFixture.cs
@@ -0,0 +1,64 @@
+﻿using Marten;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+namespace BeerSender.Domain.Tests
+{
+    public class MartenFixture : IDisposable
+    {
+        private readonly string schema = $"bstest{Guid.NewGuid().ToString().Replace("-", string.Empty)}";
+        public IDocumentStore Store { get; private set; }
+
+        public MartenFixture()
+        {
+            CreateSchema();
+            Store = DocumentStore.For(options =>
+            {
+                options.Connection(GetConnectionString());
+                options.DatabaseSchemaName = schema;
+            });
+        }
+
+        public void Dispose()
+        {
+            DropSchema();
+        }
+
+        private void CreateSchema()
+        {
+            using var connection = new NpgsqlConnection(GetConnectionString());
+            connection.Open();
+            using var command = connection.CreateCommand();
+            command.CommandText = $"CREATE SCHEMA IF NOT EXISTS {schema}";
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        private void DropSchema()
+        {
+            using var connection = new NpgsqlConnection(GetConnectionString());
+            connection.Open();
+            using var command = connection.CreateCommand();
+            command.CommandText = $"DROP SCHEMA IF EXISTS {schema} CASCADE";
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        private string? _connectionString;
+        private string GetConnectionString()
+        {
+            if (_connectionString == null)
+            {
+                var config = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                _connectionString = config.GetConnectionString("Marten");
+
+                if (_connectionString is null)
+                    throw new Exception("ConnectionString unavailable.");
+            }
+            return _connectionString;
+        }
+    }
+}
