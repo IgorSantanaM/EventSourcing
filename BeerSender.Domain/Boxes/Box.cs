76ab2a228a511211d43f28db838d4f1b46271564
IgorSantanaM <igorsantanamedeiros17@gmail.com>
2025-07-07 21:32:27 -0400

    (feat): Added versioning strategy without breaking outer face contract.

--- BeerSender.Domain/Boxes/Box.cs
+++ BeerSender.Domain/Boxes/Box.cs
@@ -10,14 +10,18 @@ namespace BeerSender.Domain.Boxes
     {
         public Guid Id { get; set; }
         public List<BeerBottle> BeerBottles { get; } = [];
-        public BoxCapacity? Capacity { get; private set; }
+        public BoxCapacity? BoxType { get; private set; }
+        public string? FriendlyName { get; private set; }
+        public ContainerType? ContainerType { get; private set; }
         public ShippingLabel? ShippingLabel { get; private set; }
         public bool IsClosed { get; private set; }
         public bool IsSent { get; private set; }
 
         public void Apply(BoxCreated @event)
         {
-            Capacity = @event.Capacity;
+            BoxType = @event.BoxType;
+            FriendlyName = @event.FriendlyName;
+            ContainerType = @event.ContainerType;
         }
 
         public void Apply(BeerBottleAdded @event)
@@ -40,7 +44,7 @@ namespace BeerSender.Domain.Boxes
             IsSent = true;
         }
 
-        public bool IsFull => BeerBottles.Count >= Capacity?.NumberOfSpots;
+        public bool IsFull => BeerBottles.Count >= BoxType?.NumberOfSpots;
     }
 
 }
